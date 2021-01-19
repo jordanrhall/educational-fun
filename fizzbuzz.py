b2cd5d2d250a6ee14af7cb1d90902b36beb1d84a
Jordan Hall <jordan.r.hall@ucdenver.edu>
2021-01-19 09:28:29 -0700

    abstracted my old approach into function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,23 +5,27 @@ Author: Jordan R. Hall, Date: 12/16/2020
 
 Main feature: script which prints (on a new line) every integer from 1 to 100
               but replacing all factors of 3 with 'fizz,' factors of 5 with 'buzz', and
-              factors of both with 'fizzbuzz' 
+              factors of both with 'fizzbuzz'
 """
 
-my_string = '1 \n'
+stop = int(1E2)
+factors = [3, 5]
+strings = ['fizz', 'buzz']
 
-for i in range(2,101):
+def FactorChecker(number:int, factor:int, message:str) -> str:
+    if number % factor == 0: return message
+    else: return ''
 
-    if i % 15 == 0: 
-        my_string += 'fizzbuzz \n'
-        
-    elif i % 3 == 0:
-        my_string += 'fizz \n'
-        
-    elif i % 5 == 0:
-        my_string += 'buzz \n'
+my_string = ''
+
+for i in range(1, stop + 1):
+    temp_string = ''
+    for factor, message in zip(factors, strings):
+        temp_string += FactorChecker(i, factor, message)
     
-    else:
-        my_string += str(i) + '\n'
+    if len(temp_string) == 0: temp_string = str(i)
+        
+    
+    my_string += temp_string + '\n'
         
 print(my_string)
