38147f98f8198a544d3bd49be7b0ad9b35c4b211
CarterWPAnkele <carterankele@gmail.com>
2021-02-06 19:07:03 -0700

    Made the coding a little cleaner

--- Cake_Sale_Challenge.py
+++ Cake_Sale_Challenge.py
@@ -11,12 +11,12 @@ cupcakePrice = float(0.40)
 macaronPrice = float(0.50)
 cheesecakePrice = float(0.70)
 
-# Step1: Input
+#Step 1: Input
 cupcakes = int(input("How many cupcakes do you plan to sell? "))
 macaron = int(input("How many macarons do you plan to sell? "))
 cheesecake = int(input("How many cheesecakes do you plan to sell? "))
 
-# Step 2: Process
+#Step 2: Process
 cupcakeTotal = cupcakes * cupcakePrice
 macaronTotal = macaron * macaronPrice
 cheesecakeTotal = cheesecake * cheesecakePrice
@@ -24,5 +24,4 @@ cheesecakeTotal = cheesecake * cheesecakePrice
 Total = float(cupcakeTotal + macaronTotal + cheesecakeTotal)
 
 #End Result
-
 print(Total)
