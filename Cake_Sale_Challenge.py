cda8e545ac009bd50750369e638c5a76b6e02657
CarterWPAnkele <carterankele@gmail.com>
2021-02-08 10:07:53 -0700

    Fixed bug where if you entered an invalid entry, the program would let you continue

--- Cake_Sale_Challenge.py
+++ Cake_Sale_Challenge.py
@@ -9,14 +9,45 @@ Created on Sat Feb  6 18:29:30 2021
 
 
 #Step 1: Input
-cupcakes = int(input("How many cupcakes do you plan to sell? "))
-macaron = int(input("How many macarons do you plan to sell? "))
-cheesecake = int(input("How many cheesecakes do you plan to sell? "))
+
+#constants in $ unit, no user input required
 cupcakePrice = float(0.40)
 macaronPrice = float(0.50)
 cheesecakePrice = float(0.70)
 
-#Step 2: Process
+#we must ensure that our entries are valid
+
+while True:    
+#check cupcake input, if it's not a whole number, gives message and restarts current loop  
+    try:       
+        cupcakes = int(input("How many cupcakes do you plan to sell? "))
+    except ValueError:
+        print('Please ensure your entry contains only numbers. No letters or special characters.')
+        continue
+    else:
+        break
+    
+while True:
+#check macaron input, if it's not a whole number, gives message and restarts current loop    
+    try:
+        macaron = int(input("How many macarons do you plan to sell? "))
+    except ValueError:
+        print('Please ensure your entry contains only numbers. No letters or special characters.')
+        continue
+    else:
+        break
+
+while True:
+#check cheesecake input, if it's not a whole number, gives message and restarts current loop      
+    try:       
+        cheesecake = int(input("How many cheesecakes do you plan to sell? "))
+    except ValueError:
+        print('Please ensure your entry contains only numbers. No letters or special characters.')
+        continue
+    else:
+        break
+#Step 2: Process 
+#multiply number of sold pastries by their respective price for total earned on each  
 cupcakeTotal = cupcakes * cupcakePrice
 macaronTotal = macaron * macaronPrice
 cheesecakeTotal = cheesecake * cheesecakePrice
