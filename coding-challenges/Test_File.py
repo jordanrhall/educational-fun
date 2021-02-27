a57d11e51de643e8ef6fd050641d55ecc2b34c7d
CarterWPAnkele <carterankele@gmail.com>
2021-02-27 15:07:14 -0700

    Added test file to demonstrate how to move changes to your github.

---
+++ coding-challenges/Test_File.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 27 13:12:54 2021
+
+@author: carte
+"""
+
+# I am making this file to test GitHub commands
+print("This is a test to show Ian Spyder and Github functions. #winning")
